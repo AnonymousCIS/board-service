9781d5ea439459ab0d19ed0daf0e1c6108b0ae57
PaB1010 <mudodd@naver.com>
2025-01-28 19:19:07 +0900

    add 설명 주석 추가 및 코드 가독성 정리

--- src/main/java/org/anonymous/global/configs/SwaggerConfig.java
+++ src/main/java/org/anonymous/global/configs/SwaggerConfig.java
@@ -6,16 +6,23 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Swagger API 설정
+ *
+ * - API 문서 자동 생성 도구
+ *
+ */
 @Configuration
-@OpenAPIDefinition(info = @Info(title="게시판 API", description = "게시판, 게시글, 댓글에 관한 API를 제공"))
+// 제목 & 설명
+@OpenAPIDefinition(info = @Info(title="Board API", description = "게시판, 게시글, 댓글 API 제공"))
 public class SwaggerConfig {
     
     @Bean
     public GroupedOpenApi apiGroup() {
 
         return GroupedOpenApi.builder()
-                .group("게시판 API v1")
-                .pathsToMatch("/**")
+                .group("Board API v1") // Group 이름 -> group("설명")
+                .pathsToMatch("/**") // 경로 패턴 지정 (api 문서에 속하는 모든 경로)
                 .build();
     }
 }
--- src/main/java/org/anonymous/global/libs/Utils.java
+++ src/main/java/org/anonymous/global/libs/Utils.java
@@ -40,6 +40,8 @@ public class Utils {
      * @return
      */
     public String getMessage(String code) {
+
+        // 요청 header 에 있는 언어 정보(Accept-Language)로 만들어지는 Locale 객체
         Locale lo = request.getLocale(); // 사용자 요청 헤더(Accept-Language)
 
         return messageSource.getMessage(code, null, lo);
@@ -58,27 +60,35 @@ public class Utils {
     }
 
     /**
-     * REST 커맨드 객체 검증 실패시에 에러 코드를 가지고 메세지 추출
+     * REST 커맨드 객체 검증 실패시에
+     * Error Code 에서 Message 추출하는 기능
      *
      * @param errors
      * @return
      */
     public Map<String, List<String>> getErrorMessages(Errors errors) {
+
+        // 형변환해도 싱글톤 객체
         ResourceBundleMessageSource ms = (ResourceBundleMessageSource) messageSource;
 
-
-            // 필드별 에러코드 - getFieldErrors()
-            // Collectors.toMap
+            // 필드별 Error Code - getFieldErrors()
+            // FieldError = 커맨드 객체 검증 실패 & rejectValue(..)
+            // Collectors.toMap = (Key = 필드명, Value = 메세지)
             Map<String, List<String>> messages = errors.getFieldErrors()
                     .stream()
                     .collect(Collectors.toMap(FieldError::getField, f -> getMessages(f.getCodes()), (v1, v2) -> v2));
+                    // v1 = 처음 값, v2 = 마지막에 들어온 값
+                    // -> 중복될 경우 마지막 값으로 대체되도록 처리 (put과 유사)
 
-            // 글로벌 에러코드 - getGlobalErrors()
+            // 글로벌 Error Code - getGlobalErrors()
+            // GlobalError = reject(..)
             List<String> gMessages = errors.getGlobalErrors()
                     .stream()
+                    // flatMap = 중첩된 stream() 펼쳐서 1차원 배열로 변환
                     .flatMap(o -> getMessages(o.getCodes()).stream())
                     .toList();
-            // 글로벌 에러코드 필드 - global
+
+            // Global ErrorCode Field = "global" 으로 임의 고정
             if (!gMessages.isEmpty()) {
                 messages.put("global", gMessages);
             }
@@ -89,8 +99,9 @@ public class Utils {
     /**
      * 유레카 서버 인스턴스 주소 검색
      *
-     *      spring.profiles.active : dev - localhost로 되어 있는 주소를 반환
-     *          - 예) member-service : 최대 2가지만 존재, 1 - 실 서비스 도메인 주소, 2. localhost ...
+     *      spring.profiles.active : dev - localhost 로 되어있는 주소 반환
+     *          - EX) member-service : 최대 두가지만 존재
+     *                                  1) 실 서비스 도메인 주소 2) localhost... (개발용)
      * @param serviceId
      * @param url
      * @return
@@ -98,22 +109,34 @@ public class Utils {
     public String  serviceUrl(String serviceId, String url) {
         try {
             List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+
             String profile = System.getenv("spring.profiles.active");
+
+            // 개발 모드 - localhost 의 Service Url
             boolean isDev = StringUtils.hasText(profile) && profile.contains("dev");
+
             String serviceUrl = null;
+
             for (ServiceInstance instance : instances) {
+
                 String uri = instance.getUri().toString();
+
                 if (isDev && uri.contains("localhost")) {
+
                     serviceUrl = uri;
+
                 } else if (!isDev && !uri.contains("localhost")) {
+
                     serviceUrl = uri;
                 }
             }
 
             if (StringUtils.hasText(serviceUrl)) {
+
                 return serviceUrl + url;
             }
         } catch (Exception e) {
+
             e.printStackTrace();
         }
 
