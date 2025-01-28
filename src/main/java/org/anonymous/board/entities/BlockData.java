39dc80dfdb9afe9da73f5968883c5944261fe614
PaB1010 <mudodd@naver.com>
2025-01-28 17:19:10 +0900

    add BlockData Entity

--- src/main/java/org/anonymous/board/entities/BlockData.java
+++ src/main/java/org/anonymous/board/entities/BlockData.java
@@ -1,26 +1,26 @@
-//package org.anonymous.board.entities;
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//import org.anonymous.board.constants.BoardStatus;
-//
-//@Data
-//@Entity
-//public class BlockData {
-//
-//    // 차단당한 컨텐츠 예시
-//
-//    @Id
-//    @GeneratedValue
-//    private Long seq;
-//
-//    private BoardStatus status;
-//
-//    @Column(nullable = false)
-//    @OneToMany
-//    private BoardData boardData;
-//
-//    @Column(nullable = false)
-//    @OneToMany
-//    private CommentData commentData;
-//}
+package org.anonymous.board.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.anonymous.board.constants.BoardStatus;
+
+@Data
+@Entity
+public class BlockData {
+
+    @Id
+    @GeneratedValue
+    private Long seq;
+
+    @Column(nullable = false)
+    private BoardStatus status;
+
+    // 차단 컨텐츠 타입
+    // 댓글 | 게시글
+    @Column(nullable = false)
+    private String type;
+
+    // 차단 회원 이메일
+    @Column(nullable = false)
+    private String email;
+}
